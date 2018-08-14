627440db76bfe778dbab45b112255be74fba4876
Marco Gasi <marqusG@users.noreply.github.com>
2018-08-14 15:17:27 +0100

    Made table_name dynamic

--- example/save.php
+++ example/save.php
@@ -5,6 +5,6 @@ require_once "../lib/FormSaver.class.php";
 $user = 'Marco/personal/';
 $item_id = filter_input(INPUT_POST, 'item_id', FILTER_SANITIZE_NUMBER_INT);
 // $fs = new FormSaver($item_id); //using root_target_dir as specified in config file
-$fs = new FormSaver($user, $item_id); //passing dinamically a root_target_dir
+$fs = new FormSaver('products', $user, $item_id); //passing dinamically a root_target_dir
 $fs->save();
 header('Location: manage.php');
